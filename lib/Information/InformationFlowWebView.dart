475a4c272a273dd5e52e7bf677b68129c8a7c62b
zhujilong <zhujilong2007@sina.com>
2020-04-17 06:50:48 +0800

    使用新的框架webview_flutter,注意需要在iOS的info.plist文件中进行配置.

--- lib/Information/InformationFlowWebView.dart
+++ lib/Information/InformationFlowWebView.dart
@@ -1,14 +1,14 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_html/flutter_html.dart';
-
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import 'package:play_android/Responses/InformationFlowProtocol.dart';
-
 import 'package:play_android/Compose/Marquee.dart';
 
 class InformationFlowWebView extends StatefulWidget {
@@ -20,6 +20,9 @@ class InformationFlowWebView extends StatefulWidget {
 class _InformationFlowWebViewState extends State<InformationFlowWebView> {
   FlutterWebviewPlugin _flutterWebviewPlugin = FlutterWebviewPlugin();
 
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   InformationFlowProtocol _model;
 
   var _isLoading = true;
@@ -27,7 +30,7 @@ class _InformationFlowWebViewState extends State<InformationFlowWebView> {
   @override
   void initState() {
     super.initState();
-    _startWebViewListen();
+    //_startWebViewListen();
   }
 
   Widget marqueeText() {
@@ -39,34 +42,87 @@ class _InformationFlowWebViewState extends State<InformationFlowWebView> {
   }
 
   Widget normalText() {
-    return SingleChildScrollView(child: Text(_model.title, style: TextStyle(color: Colors.white)),scrollDirection: Axis.horizontal,);
+    return SingleChildScrollView(
+      child: Text(_model.title, style: TextStyle(color: Colors.white)),
+      scrollDirection: Axis.horizontal,
+    );
   }
 
   Widget loading() {
     return Container(
-      width: 10, 
-      height: 10, 
-      child: CupertinoActivityIndicator()
-    );
+        width: 10, height: 10, child: CupertinoActivityIndicator());
   }
 
   @override
   Widget build(BuildContext context) {
     _model = ModalRoute.of(context).settings.arguments;
+    return scaffold();
+  }
 
+  AppBar appBar() {
+    return AppBar(
+      // 参考了微信的做法,直接就没有标题,这个title有h5元素,
+      //用Text或者框架Html都不能很好解决,加上跑马灯的问题
+      //title: normalText(),
+      iconTheme: IconThemeData(color: Colors.white),
+      elevation: 0.1,
+      actions: <Widget>[
+        _isLoading ? loading() : Container(),
+        IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
+      ],
+    );
+  }
+
+  Scaffold scaffold() {
+    return Scaffold(
+      appBar: appBar(),
+      // We're using a Builder here so we have a context that is below the Scaffold
+      // to allow calling Scaffold.of(context) so we can show a snackbar.
+      body: SafeArea(
+        child: Builder(builder: (BuildContext context) {
+          return WebView(
+            initialUrl: _model.link,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            // Remove this when collection literals makes it to stable.
+            // ignore: prefer_collection_literals
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.startsWith('https://www.youtube.com/')) {
+                print('blocking navigation to $request}');
+                return NavigationDecision.prevent;
+              }
+              print('allowing navigation to $request');
+              return NavigationDecision.navigate;
+            },
+            onPageStarted: (String url) {
+              print('Page started loading: $url');
+              setState(() {
+                _isLoading = true;
+              });
+            },
+            onPageFinished: (String url) {
+              print('Page finished loading: $url');
+              setState(() {
+                _isLoading = false;
+              });
+            },
+            gestureNavigationEnabled: true,
+          );
+        }),
+      ),
+    );
+  }
+
+  /* 
+  以下方法没有用到
+   */
+
+  WebviewScaffold webviewPlugin() {
     return WebviewScaffold(
       url: _model.link,
-      appBar: AppBar(
-        // 参考了微信的做法,直接就没有标题,这个title有h5元素,
-        //用Text或者框架Html都不能很好解决,加上跑马灯的问题
-        //title: normalText(),
-        iconTheme: IconThemeData(color: Colors.white),
-        elevation: 0.1,
-        actions: <Widget>[
-          _isLoading ? loading() : Container(),
-          IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
-        ],
-      ),
+      appBar: appBar(),
       withJavascript: true,
       //允许执行js
       withLocalStorage: true,
