c954d7ace2a79cc230e512ebb21f52fa2a12c99a
zhujilong <zhujilong2007@sina.com>
2020-04-16 09:29:33 +0800

    Dart泛型的协议化思考

--- lib/HttpUtils/HttpUtils.dart
+++ lib/HttpUtils/HttpUtils.dart
@@ -27,3 +27,17 @@ abstract class HttpUtils {
     return response.data;
   }
 }
+
+extension BaseRequest on HttpUtils {
+  /* Dart的语言限制了泛型更广泛的使用,Future包裹的需要的是一个具体类型,而下面的这种写法无法通过
+  static Future<T implements BaseResponse> getBaseResponse({String api, Map<String, dynamic> params}) async {
+    Map<String, dynamic> json = await get(api: api, params: params);
+    return BaseResponse(json);
+  }
+
+  static Future<T implements BaseResponse> postBaseResponse({String api, Map<String, dynamic> params}) async {
+    Map<String, dynamic> json = await post(api: api, params: params);
+    return BaseResponse(json);
+  }
+   */
+}
--- lib/Responses/BaseResponse.dart
+++ lib/Responses/BaseResponse.dart
@@ -1,6 +1,8 @@
 /* 思考如果通过协议进行泛型化 */
-abstract class BaseResponse {
-  BaseResponse();
+abstract class BaseResponse<T> {
+  T data;
+  int errorCode;
+  String errorMsg;
 
   factory BaseResponse.fromJson(Map<String, dynamic> json) {
     return null;
