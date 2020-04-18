bd2118a148bf3e6a9053a48937720749366e1147
zhujilong <zhujilong2007@sina.com>
2020-04-18 09:04:58 +0800

    编写了一个Space,对于已知宽高的情况下使用很好

---
+++ lib/Compose/Space.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// 如果你使用过SwiftUI,一定会发现这是我夹带的私货,对于布局有一定的帮助
+class Space extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Container(),);
+  }
+}
