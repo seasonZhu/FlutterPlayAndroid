b965bb12a3daee954d2ef868df80d27b6a22994d
zhujilong <zhujilong2007@sina.com>
2020-04-20 20:20:00 +0800

    Space的构造器优化

--- lib/Compose/Space.dart
+++ lib/Compose/Space.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 // 如果你使用过SwiftUI,一定会发现这是我夹带的私货,对于布局有一定的帮助
 class Space extends StatelessWidget {
+
+  final int _flex;
+
+  Space({Key key, int flex = 1}):
+    _flex = flex,
+    super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: Container(),);
+    return Expanded(child: Container(), flex: _flex);
   }
 }
