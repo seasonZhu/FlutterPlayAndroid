4f9e8ad9b9da3120a8075eda2137b0b5e6e713e8
zhujilong <zhujilong2007@sina.com>
2020-04-20 09:38:46 +0800

    请求的状态枚举

---
+++ lib/Compose/RequestState.dart
@@ -0,0 +1,14 @@
+// Dart的枚举功能还不够强大,居然不能进行嵌套使用
+// 对于一个UI = f(state)的框架,枚举真的很重要
+// 和Swift的枚举比真的太弱鸡了
+
+enum RequestState {
+  success,
+  loading,
+  fail
+}
+
+enum RequestSuccessState {
+  hasContent,
+  empty
+}
