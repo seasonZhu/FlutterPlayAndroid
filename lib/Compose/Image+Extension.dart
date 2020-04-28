e023ebb2319ef1c75fc73747897a8f24bd160402
zhujilong <zhujilong2007@sina.com>
2020-04-28 15:53:45 +0800

    开始写分类了

---
+++ lib/Compose/Image+Extension.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+extension Local on Image {
+  static var path = "assets/images/";
+
+  static Image local({@required String name, String format = "png"}) {
+    var imagePath = '$path$name.$format';
+    return Image.asset(imagePath);
+  }
+}
