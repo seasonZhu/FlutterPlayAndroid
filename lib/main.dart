290ba0682eb4d192f75d9c8431c5503b316f9ace
zhujilong <zhujilong2007@sina.com>
2020-05-12 13:55:30 +0800

    强制竖屏

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 //import 'View/BlocExampleApp.dart';
 import 'View/PlayAndroidApp.dart';
 
-void main() => runApp(PlayAndroidApp());
+void main() {
+  // 强制竖屏
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(PlayAndroidApp());
+}
 
 
