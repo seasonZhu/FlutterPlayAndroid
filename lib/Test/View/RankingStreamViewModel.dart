bf1b86b63b3e77703f5987150f8cb871eda0dc85
zhujilong <zhujilong2007@sina.com>
2020-07-02 10:04:18 +0800

    尝试编写通过的ViewModel,泛型仅能支持extends的悲剧

--- lib/Test/View/RankingStreamViewModel.dart
+++ lib/Test/View/RankingStreamViewModel.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:play_android/HttpUtils/Request.dart';
+import 'package:play_android/Responses/BaseResponse.dart';
 import 'package:play_android/Responses/RankListResponse.dart';
 
 import 'RankingStreamViewEvent.dart';
@@ -83,3 +84,83 @@ class RankingStreamViewModel {
     _stateController.close();
   }
 }
+
+/*
+/// 尝试编写play_android整体架构中的通用ViewModel层
+class PAViewModel<State extends StreamState, Element, Event extends StreamEvent, Response extends BaseResponse> {
+final StreamController<State> _stateController =
+      StreamController<State>();
+
+  List<Element> data = [];
+
+  num _page = 1;
+
+  Stream<State> get stream => _stateController.stream;
+
+  Sink<State> get _sink => _stateController.sink;
+
+  void dispatch(Event event) {
+    print('Event dispatched: $event');
+    if (event is FetchDataEvent) {
+      _request(event.type);
+    }
+  }
+
+  void _request(ActionType type) {
+    switch (type) {
+      case ActionType.refresh:
+        _onRefresh();
+        break;
+      case ActionType.pullUp:
+        _onLoading();
+        break;
+    }
+  }
+
+  Future<Response> _getRankList() async {
+    var model = await Request.getRankingList(page: _page);
+    return model;
+  }
+
+  Future _onRefresh() async {
+    _page = 1;
+    var model = await _getRankList();
+    if (model.errorCode == 0) {
+      data.clear();
+      data.addAll(model.data.datas);
+      if (data.length > 0) {
+        _sink.add(HasData(
+            completeType: RequestCompleteState.refreshCompleted, data: data));
+      } else {
+        _sink.add(NoDataState());
+      }
+    } else {
+      _sink.add(ErrorState());
+    }
+  }
+
+  Future _onLoading() async {
+    _page = _page + 1;
+    var model = await _getRankList();
+    if (model.errorCode == 0) {
+      data.addAll(model.data.datas);
+      if (model.data.pageCount == model.data.curPage) {
+        _sink.add(
+            HasData(completeType: RequestCompleteState.loadNoData, data: data));
+      } else {
+        _sink.add(HasData(
+            completeType: RequestCompleteState.loadComplete, data: data));
+      }
+    } else {
+      _sink.add(
+          HasData(completeType: RequestCompleteState.loadComplete, data: []));
+    }
+  }
+
+  /// StreamController一般用完都是需要在dispose方法中close的 但是这种没有继承Widget的根本就没有这个方法
+  /// 既然工具类没有这个方法,就自己写一个,在Widget层进行释放
+  void dispose() {
+    _stateController.close();
+  }
+}
+*/
