e518c08ddd6ac1d39d82eed005e37a5c86c427b6
zhujilong <zhujilong2007@sina.com>
2021-02-19 10:23:56 +0800

    放开测试栏

--- lib/Compose/FrostedGlassView.dart
+++ lib/Compose/FrostedGlassView.dart
@@ -10,20 +10,20 @@ class FrostedGlassView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: <Widget>[
-          child,
-          BackdropFilter(
-            /// 建议sigmaX与sigmaY参数都为5 这个效果最好
-            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-            child: Opacity(
-              /// opacity为0.2,数值过大就完全遮挡了
-              opacity: 0.2,
-              child: Container(
-                decoration: BoxDecoration(color: Colors.grey.shade200),
-              ),
+      children: <Widget>[
+        child,
+        BackdropFilter(
+          /// 建议sigmaX与sigmaY参数都为5 这个效果最好
+          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+          child: Opacity(
+            /// opacity为0.2,数值过大就完全遮挡了
+            opacity: 0.2,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.grey.shade200),
             ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
